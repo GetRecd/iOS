72619c5a8a15699fa3d80966e11280a6f445674e
Dhruv Upadhyay <1997dhruv@gmail.com>
2018-02-20 00:59:28 -0500

    profile page in progress

---
+++ GetRecd/Controllers/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  GetRecd
+//
+//  Created by Dhruv Upadhyay on 2/16/18.
+//  Copyright © 2018 CS 407. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+}
