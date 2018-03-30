9c059bebec55484fb8d87b1c0c0a86eec5773514
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2018-03-30 11:19:42 -0400

    Implemented back-end functions for friends.

--- GetRecd/Services/DataService.swift
+++ GetRecd/Services/DataService.swift
@@ -20,6 +20,7 @@ class DataService {
     private var _REF_USERS = Database.database().reference().child("Users")
     private var _REF_USERS_LIKES = Database.database().reference().child("UsersLikes")
     private var _REF_USERS_FRIENDS = Database.database().reference().child("UsersFriends")
+    private var _REF_USERS_PENDING_FRIENDS = Database.database().reference().child("UsersPendingFriends")
     private var _REF_USERS_SPOTIFY_PLAYLISTS = Database.database().reference().child("UsersSpotifyPlaylists")
 
     // Firebase Storage references.
@@ -61,11 +62,11 @@ class DataService {
     }
     
     /**
-      * Retrieves a user's profile picture.
-      *
-      * - parameter user: The user whose photo is to be retrieved.
-      * - parameter handler: The handler that will be invoked upon a successful `UIImage` retrieval.
-      */
+     * Retrieves a user's profile picture.
+     *
+     * - parameter user: The user whose photo is to be retrieved.
+     * - parameter handler: The handler that will be invoked upon a successful `UIImage` retrieval.
+     */
     func getProfilePicture(user: User, handler: @escaping (_ image: UIImage) -> ()) {
         guard let url = URL(string: user.profilePictureURL) else {
             return
@@ -98,34 +99,180 @@ class DataService {
     }
     
     /**
-      * Deletes a user from the database.
-      *
-      * - parameter uid: The (unique) user ID.
-      */
+     * Deletes a user from the database.
+     *
+     * - parameter uid: The (unique) user ID.
+     */
     func deleteUser(uid: String) {
         print("Deleting user with UID: \(uid)")
         _REF_USERS.child(uid).removeValue()
     }
     
     /**
-     * Returns an array of `User` objects whose names contain `nameSubstring`.
+     * Returns an array of `User` objects whose names contain `nameSubstring`. The users are
+     * supplied to the callback handler.
      *
      * - parameter nameSubstring: A substring of a user's name to match.
+     * - parameter handler: The callback handler that will be invoked with the matching users.
      */
-    func findUsersByName(nameSubstring: String) -> [User] {
-        var matchingUsers = [User]()
+    func findUsersByName(nameSubstring: String, handler: @escaping (_ matchingUsers: [User]) -> ()) {
         _REF_USERS.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
+            var matchingUsers = [User]()
             if let userEntries = snapshot.value as? Dictionary<String, AnyObject> {
-                for uid in userEntries.keys {
-                    self.getUser(uid: uid) { (user) in
-                        if user.name.lowercased().contains(nameSubstring.lowercased()) {
-                            matchingUsers.append(user)
+                for (uid, userDictionary) in userEntries {
+                    matchingUsers.append(User(userDict: userDictionary as! [String : Any], userID: uid))
+                }
+                handler(matchingUsers)
+            }
+        })
+    }
+    
+    /**
+     * Adds a friend request entry for the user specified by their ID from the currently
+     * authenticated user.
+     *
+     * - parameter friendUid: The user ID of the friend the current user requested to add.
+     */
+    func requestFriend(friendUid: String) {
+        if let currentUid = Auth.auth().currentUser?.uid {
+            if currentUid == "" || friendUid == "" {
+                return
+            } else if friendUid == currentUid {
+                print("Attempting to friend request oneself.")
+                return
+            }
+            _REF_USERS_PENDING_FRIENDS.child(currentUid).observeSingleEvent(of: .value, with: { (snapshot) in
+                var pendingUids = [String]()
+                if snapshot.exists() {
+                    if let friendUids = snapshot.value as? [String] {
+                        pendingUids += friendUids
+                    }
+                }
+                pendingUids.append(friendUid)
+                self._REF_USERS_PENDING_FRIENDS.child(currentUid).setValue(pendingUids)
+            })
+        }
+    }
+    
+    /**
+     * Returns an array of `User` objects corresponding to users that have requested to be
+     * friends with the current user. The users are supplied to the callback handler.
+     *
+     * - parameter handler: The callback handler that will be invoked with the incoming friends.
+     */
+    func getIncomingFriendRequests(handler: @escaping (_ incomingFriends: [User]) -> ()) {
+        if let currentUid = Auth.auth().currentUser?.uid {
+            if currentUid == "" {
+                return
+            }
+            _REF_USERS_PENDING_FRIENDS.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
+                if let requestEntries = snapshot.value as? Dictionary<String, AnyObject> {
+                    for (uid, friendUids) in requestEntries {
+                        if let friendUidsArray = friendUids as? [String] {
+                            var incomingFriends = [User]()
+                            for friendUid in friendUidsArray {
+                                if currentUid == friendUid {
+                                    self.getUser(uid: uid, handler: { (user) in
+                                        incomingFriends.append(user)
+                                        handler(incomingFriends)
+                                    })
+                                }
+                            }
                         }
                     }
                 }
+            })
+        }
+    }
+    
+    /**
+     * Returns an array of `User` objects corresponding to users that the current user has
+     * requested to be friends with. The users are supplied to the callback handler.
+     *
+     * - parameter handler: The callback handler that will be invoked with the outgoing friends.
+     */
+    func getOutgoingFriendRequests(handler: @escaping (_ outgoingFriends: [User]) -> ()) {
+        if let currentUid = Auth.auth().currentUser?.uid {
+            if currentUid == "" {
+                return
             }
-        })
-        return matchingUsers
+            _REF_USERS_PENDING_FRIENDS.queryOrderedByKey().observeSingleEvent(of: .value, with: { (snapshot) in
+                if let requestEntries = snapshot.value as? Dictionary<String, AnyObject> {
+                    if let friendUids = requestEntries[currentUid] as? [String] {
+                        var outgoingFriends = [User]()
+                        for friendUid in friendUids {
+                            self.getUser(uid: friendUid, handler: { (user) in
+                                outgoingFriends.append(user)
+                                handler(outgoingFriends)
+                            })
+                        }
+                    }
+                }
+            })
+        }
+    }
+    
+    /**
+     * Returns an array of `User` objects that the current user is friends with. The users are
+     * supplied to the callback handler.
+     *
+     * - parameter handler: The callback handler that will be invoked with the user's friends.
+     */
+    func getFriends(handler: @escaping (_ friends: [User]) -> ()) {
+        if let currentUid = Auth.auth().currentUser?.uid {
+            if currentUid == "" {
+                return
+            }
+            _REF_USERS_FRIENDS.child(currentUid).observeSingleEvent(of: .value, with: { (snapshot) in
+                if let friendUids = snapshot.value as? [String] {
+                    var friends = [User]()
+                    for friendUid in friendUids {
+                        self.getUser(uid: friendUid, handler: { (user) in
+                            friends.append(user)
+                            handler(friends)
+                        })
+                    }
+                }
+            })
+        }
+    }
+    
+    /**
+     * Responds to a friend request from the user with the ID `requesterUid`. If the `accept`
+     * argument is `true`, then the requester is added to the current user's friends list. In
+     * both cases, the pending friend request is removed as a result of this call.
+     *
+     * - parameter requesterUid: The user ID of the person that requested to be friends.
+     * - parameter accept: If the friend request was accepted.
+     */
+    func respondFriendRequest(requesterUid: String, accept: Bool) {
+        if let currentUid = Auth.auth().currentUser?.uid {
+            if currentUid == "" {
+                return
+            }
+            _REF_USERS_PENDING_FRIENDS.child(requesterUid).observeSingleEvent(of: .value, with: { (snapshot) in
+                if let pendingUids = snapshot.value as? [String] {
+                    if let currentUserIndex = pendingUids.index(of: currentUid) {
+                        var removedUids = [String]()
+                        removedUids += pendingUids
+                        removedUids.remove(at: currentUserIndex)
+                        self._REF_USERS_PENDING_FRIENDS.child(requesterUid).setValue(removedUids)
+                    }
+                }
+            })
+            if accept {
+                _REF_USERS_FRIENDS.child(currentUid).observeSingleEvent(of: .value, with: { (snapshot) in
+                    var appendedUids = [String]()
+                    if snapshot.exists() {
+                        if let friendUids = snapshot.value as? [String] {
+                            appendedUids += friendUids
+                        }
+                    }
+                    appendedUids.append(requesterUid)
+                    self._REF_USERS_FRIENDS.child(currentUid).setValue(appendedUids)
+                })
+            }
+        }
     }
 
     func setUserSpotifyPlaylist(uid: String, uri: String, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
